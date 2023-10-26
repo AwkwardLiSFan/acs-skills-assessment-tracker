8c75d6c6d37df3932042a147e47b40ad2b76790d
Soham Sevak <soham.sevak@consilium.technology>
2023-10-26 19:35:44 +1030

    refactor: fetch data in home component, pass to child component via input

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,32 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { MatTableDataSource } from "@angular/material/table";
+import { map } from "rxjs";
+import { GetAllEntriesGQL } from "src/app/graphql/graphql-codegen-generated";
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.scss"],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  public dataSource: MatTableDataSource<any> = new MatTableDataSource();
+
+  constructor(private getAllEntriesQuery: GetAllEntriesGQL) {}
+
+  ngOnInit(): void {
+    console.log(`Placeholder comment to pass linting, remove before pushing`);
+    // this.fetchTableEntries();
+  }
+
+  /**
+   * Runs GraphQL query to fetch all table entries from the MongoDB Atlas Collection
+   */
+  private fetchTableEntries(): void {
+    this.getAllEntriesQuery
+      .fetch()
+      .pipe(map((response) => response.data.applications))
+      .subscribe((data) => {
+        this.dataSource = new MatTableDataSource(data);
+      });
+  }
+}
