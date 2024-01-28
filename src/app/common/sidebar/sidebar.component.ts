557079eba3f5186d543191f12ca02bd2fcf354b5
Soham Sevak <soham.sevak@consilium.technology>
2024-01-28 18:42:53 +1030

    feat: add new sidebar

--- src/app/common/sidebar/sidebar.component.ts
+++ src/app/common/sidebar/sidebar.component.ts
@@ -1,8 +1,33 @@
 import { Component } from "@angular/core";
+import { IconDefinition, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faQuestionCircle } from "@fortawesome/free-solid-svg-icons";
+import { environment } from "src/environments/environment";
+
+export interface RouteLink {
+  displayName: string;
+  path: string;
+}
 
 @Component({
   selector: "app-sidebar",
   templateUrl: "./sidebar.component.html",
   styleUrls: ["./sidebar.component.scss"],
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  darkLogoUrl: string =
+    environment.iconsDeployUrl + "assets/images/ACSTrackerDarkLogo.png";
+  title = "acs tracker";
+  githubIcon: IconDefinition = faGithub;
+  helpIcon: IconDefinition = faQuestionCircle;
+
+  links: RouteLink[] = [
+    { displayName: "HOME", path: "/home" },
+    { displayName: "LOG", path: "/log" },
+    { displayName: "VISUALISE", path: "/visualise" },
+    { displayName: "ASK", path: "/ask" },
+  ];
+
+  constructor() {
+    console.log(`Sidebar WIP`);
+  }
+}
